4eeaf911892b648b893875d9f8db65ee64b18089
davidquero <jose.david.quero.sanchez@gmail.com>
2017-12-19 09:46:33 +0100

    no message

---
+++ Ejemplo1/Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float Speed = 0.5f;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * Speed, 0, 0);
+	}
+}
